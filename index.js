9fbd27456f940c719d10cf948aec62b98f3feb2c
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-17 08:40:35 -0600

    save

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function validateAge(oldEnough){
   // add your code here
 }
+
+should add logic to the function named validateAge so that it returns "old enough" if oldEnough is true
+should not return "old enough" from the validateAge function if oldEnough is false
