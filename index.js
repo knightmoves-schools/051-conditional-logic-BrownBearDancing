f6bd190195cdcc43e7ffe6051273760043910ccb
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-18 14:23:38 -0600

    forgot outside bracket

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function validateAge(oldEnough){
-  if (oldEnough === true {
+  if (oldEnough === true) {
     return "old enough";
   }
 }
 
 //should add logic to the function named validateAge so that it returns "old enough" if oldEnough is true
 //should not return "old enough" from the validateAge function if oldEnough is false
+
 
 
 
