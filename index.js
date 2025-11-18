b09fa69bac5124d3cd883d6ab04238fd5dd7a597
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-11-18 14:21:34 -0600

    if then

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 function validateAge(oldEnough){
-  let validateAge = true;
-  if (validateAge) {
-    document.getElementById("result").innerHTML = 'old enough';
-  }
-    if (validateAge) = false {
-    document.getElementById("result").innerHTML = 'not old enough';
+  if (oldEnough === true {
+    return "old enough";
   }
 }
 
 //should add logic to the function named validateAge so that it returns "old enough" if oldEnough is true
 //should not return "old enough" from the validateAge function if oldEnough is false
+
 
 
 
